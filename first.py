458717ccd07ea6acb381cc6b5645427c5c5d3379
Anton Bazhenov <horikovich@yandex.ru>
2024-06-11 15:07:56 +0300

    fix bugs after test

--- first.py
+++ first.py
@@ -7,19 +7,31 @@ class BankAccount(object):
         self.created_date = date.today()
 
     def deposit(self, amount):
-        self.balance += amount
+        if self.is_integer_and_positive(amount):
+            self.balance += amount
 
     def is_enough_to_withdraw(self, amount: int) -> bool:
-        to_return = True if self.balance - amount > 0 else False
+        to_return = True if self.balance - amount >= 0 else False
         return to_return
     
+    def is_integer_and_positive(self, amount):
+        to_return = True if isinstance(amount, int) and amount > -1 else False
+        return to_return
+
     def withdraw(self, amount):
-        if self.is_enough_to_withdraw(amount):
-            self.balance -= amount
+        if self.is_integer_and_positive(amount):
+            if self.is_enough_to_withdraw(amount):
+                self.balance -= amount
+                return True
+        return False
 
     def transfer_from(self, account, amount):
-        account.withdraw(amount)
-        self.deposit(amount)
+        liq = account.withdraw(amount)
+        if liq is True:
+            self.deposit(amount)
+
+    def __repr__(self):
+        return 'number: {0}, balance: {1}, created date: {2}'.format(self.number, self.balance, self.created_date)
 
     def __str__(self):
         return 'number: {0}, balance: {1}, created date: {2}'.format(self.number, self.balance, self.created_date)
--- test_oop.py
+++ test_oop.py
@@ -6,10 +6,13 @@ class TestBankAccount(unittest.TestCase):
         self.account = BankAccount(100)
 
     def test_account_created(self):
-        temp_date = date.today()
         test_balance = 99
-        self.test_account = BankAccount(99)
-        self.assertEqual(self.test_account, f'number: {1}, balance: {test_balance}, created date: {temp_date}')
+        test_account = BankAccount(test_balance)
+        self.assertIsInstance(test_account, BankAccount)
+
+    def test_invalid_account_created(self):
+        test_account = BankAccount()
+        self.assertRaises(TypeError)
 
     def test_counter_number_accounts_with_del(self):
         ...
@@ -18,28 +21,61 @@ class TestBankAccount(unittest.TestCase):
         ...
 
     def test_update_account_balance(self):
-        ...
+        test_balance = 99
+        test_deposit = 1
+        test_account = BankAccount(99)
+        test_account.deposit(test_deposit)
+        self.assertEqual(test_account.balance, 100)
     
-    def test_invalid_update_account_balance(self):
-        ...
+    def test_invalid_account_deposit_with_less_zero_int(self):
+        test_balance = 99
+        test_deposit = -1
+        test_account = BankAccount(99)
+        test_account.deposit(test_deposit)
+        self.assertEqual(test_account.balance, 99)
 
-    def test_invalud_account_created(self):
-        ...
-    
-    def test_invalid_account_deposit(self):
-        ...
+    def test_invalid_update_account_balance_with_str(self):
+        test_balance = 99
+        test_deposit = '-1'
+        test_account = BankAccount(test_balance)
+        test_account.deposit(test_deposit)
+        self.assertRaises(Exception)
 
-    def test_account_deposit(self):
-        test_balance = 150
-        self.account.deposit(50)
-        self.assertEqual(self.account.balance, test_balance)
+    def test_account_transfer(self):
+        test_balance = 99
+        test_withdraw = 99
+        test_account_1 = BankAccount(test_balance)
+        test_account_2 = BankAccount(test_balance)
+        test_account_2.transfer_from(test_account_1, test_withdraw)
+        self.assertEqual(test_account_1.balance, 0)
+        self.assertEqual(test_account_2.balance, 198)
 
-    def test_account_withdraw(self):
-        ...
+    def test_invalid_account_transfer_with_less_zero_int(self):
+        test_balance = 99
+        test_withdraw = -1
+        test_account_1 = BankAccount(test_balance)
+        test_account_2 = BankAccount(test_balance)
+        test_account_2.transfer_from(test_account_1, test_withdraw)
+        self.assertEqual(test_account_1.balance, 99)
+        self.assertEqual(test_account_2.balance, 99)
 
-    def test_invalid_account_withdraw(self):
-        ...
-
+    def test_invalid_account_transfer_with_bigger_then_account_have(self):
+        test_balance = 99
+        test_withdraw = 101
+        test_acc_1 = BankAccount(test_balance)
+        test_acc_2 = BankAccount(test_balance)
+        test_acc_2.transfer_from(test_acc_1, test_withdraw)
+        self.assertEqual(test_acc_1.balance, 99)
+        self.assertEqual(test_acc_2.balance, 99)
+  
+    def test_invalid_account_transfer_with_less_with_str(self):
+        test_balance = 99
+        test_withdraw = '101'
+        test_account_1 = BankAccount(test_balance)
+        test_account_2 = BankAccount(test_balance)
+        test_account_2.transfer_from(test_account_1, test_withdraw)
+        self.assertEqual(test_account_1.balance, 99)
+        self.assertEqual(test_account_2.balance, 99)
 
 if __name__ == '__main__':
     unittest.main()
