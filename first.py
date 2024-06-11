3f650fa9d259919375df6bbfdb63814628339cd0
Anton Bazhenov <horikovich@yandex.ru>
2024-06-11 13:51:31 +0300

    first lab (21)

--- first.py
+++ first.py
@@ -1,1 +1,11 @@
-print('start')
+class BankAccount(object):
+    def __init__(self, number, balance):
+        self.number = number
+        self.balance = balance
+    def deposit(self, amount):
+        self.balance += amount
+    
+if __name__ == '__main__':
+    ba = BankAccount(1, 100)
+    ba.deposit(50)
+    print(ba.balance)
