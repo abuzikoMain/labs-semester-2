d6ff23c94ea6ee7e045985bcea2e741aef4e1aab
Anton Bazhenov <horikovich@yandex.ru>
2024-06-13 14:45:18 +0300

    done lab number '23'

--- first.py
+++ first.py
@@ -1,21 +1,67 @@
-from datetime import date
+from datetime import date, datetime
+import pickle
+
+class PersistenceAccount(object):
+    @staticmethod
+    def serialize(account):
+        with open('bank_account.pkl', 'wb') as f:
+            pickle.dump(account, f)
+        f.closed
+    @staticmethod
+    def deserialize():
+        with open('bank_account.pkl', 'rb') as f:
+            account = pickle.load(f)
+        f.closed
+        return account
+    
+class BankTransaction(object):
+    def __init__(self, amount = 100, type_transaction = 'test_transaction', old_value = 0, new_value = 100):
+        self.when = datetime.today()
+        self.amount = amount
+        self.type_transaction = type_transaction
+        self.old_value = old_value
+        self.new_value = new_value
+
+    def __str__(self):
+        return 'type {0} | old amount {1} | new amount {2} | amount {3} | when {4}\n'\
+            .format(self.type_transaction, self.old_value, self.new_value, self.amount, self.when)
+   
+    def __del__(self):
+        with open('transaction.txt', 'a') as f:
+            f.write(str(self))
+        f.closed
 
 class BankAccount(object):
     def __init__(self, balance = 0):
         self.number = _next_number()
         self.balance = balance
+        self.queue = []
         self.created_at = date.today()
+
+    @classmethod
+    def create_bank_account(cls, value):
+        return cls(value)
 
     def deposit(self, amount):
         self.balance += amount
+        old_value = self.balance - amount
+        self.queue.append(BankTransaction(amount=amount, type_transaction='deposit', \
+                                           old_value=old_value, new_value=self.balance))
 
     def withdraw(self, amount):
-        if self.balance - amount > 0:
-            self.balance -= amount
-    
+        self.balance -= amount
+        old_value = self.balance + amount
+        self.queue.append(BankTransaction(amount=amount, type_transaction='withdraw', \
+                                           old_value=old_value, new_value=self.balance))
+
     def transfer_from(self, account, amount):
         account.withdraw(amount)
         self.deposit(amount)
+
+    def get_transaction(self):
+        for _ in range(len(self.queue)):
+            item = self.queue.pop(0)
+            print(item)
 
     def __str__(self):
         return 'number: {0}, balance: {1}'.format(self.number, self.balance)
@@ -35,6 +81,14 @@ def test_deposit(account):
     account.deposit(amount)
     print(account)
 
+def test_withdraw(account):
+    print(account)
+    amount = int(input('enter amount to withdraw from number {0}:'.format(account.number)))
+    account.withdraw(amount)
+    print(account)
+
 if __name__ == '__main__':
-    ba = BankAccount(100)
+    ba = BankAccount()
     test_deposit(ba)
+    test_withdraw(ba)
+    ba.get_transaction()
--- test_oop.py
+++ test_oop.py
@@ -1,4 +1,4 @@
-from first import BankAccount
+from first import BankAccount, PersistenceAccount, BankTransaction
 import unittest
 class TestBankAccount(unittest.TestCase):
     def setUp(self):
@@ -24,6 +24,28 @@ class TestBankAccount(unittest.TestCase):
 
     def test_account_number(self):
         self.assertEqual(self.account.number, 3)
+
+class TestBankTransaction(unittest.TestCase):
+    def setUp(self):
+        self.BankTransaction = BankTransaction() 
+
+    def test_dundor_method_del(self):
+        obj_str = str(self.BankTransaction)
+        del self.BankTransaction
+
+        with open('transaction.txt', 'r') as f:
+            lst_text = f.readlines()
+        self.assertEqual(obj_str, lst_text[-1])
+
+class TestPersistenceAccount(unittest.TestCase):
+    def setUp(self):
+        self.account = BankAccount(100)
+    
+    def test_serialisation(self):
+        PersistenceAccount.serialize(self.account)
+        from_file = PersistenceAccount.deserialize()
+        self.assertEqual(str(self.account), str(from_file))
+
 
 if __name__ == '__main__':
     unittest.main()
